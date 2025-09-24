cabbb1ae7a927ffe3009e261d8411b77f3b47ddc
Breuß Valentin <valentin.breuss@baur.eu>
2025-09-24 08:58:27 +0200

    Fix sonar issues

--- Source/aweXpect.Mocks/Setup/MockSetup.cs
+++ Source/aweXpect.Mocks/Setup/MockSetup.cs
@@ -52,9 +52,11 @@ public class MockSetup<T>(Mock<T> mock) : IMockSetup
 	void IMockSetup.RegisterSetup(MockSetup mockSetup)
 		=> ((IMockSetup)mock).RegisterSetup(mockSetup);
 
+	/// <inheritdoc cref="IMockSetup.Execute{TResult}(string, object[])" />
 	public TResult Execute<TResult>(string name, params object[] args)
 		=> ((IMockSetup)mock).Execute<TResult>(name, args);
 
+	/// <inheritdoc cref="IMockSetup.Execute(string, object[])" />
 	public void Execute(string name, params object[] args)
 		=> ((IMockSetup)mock).Execute(name, args);
 }
--- Tests/aweXpect.Mocks.Tests/MyUserRepository.cs
+++ Tests/aweXpect.Mocks.Tests/MyUserRepository.cs
@@ -1,14 +1,14 @@
-﻿namespace aweXpect.Mocks.Tests;
+﻿using System.Collections.Generic;
+
+namespace aweXpect.Mocks.Tests;
 
 public class MyUserRepository
 {
+	private readonly List<string> _users = new();
+
 	public void AddUser(string email)
-	{
-		
-	}
+		=> _users.Add(email);
 
 	public virtual bool RemoveUser(string email)
-	{
-		return false;
-	}
+		=> _users.Remove(email);
 }
